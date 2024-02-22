929b649724d8bc2a1e4e7553ff809a271a00eee2
LeJun0v0 <137397455+LeJun0v0@users.noreply.github.com>
2024-02-22 20:30:42 +0800

    Delete run directory

--- run/start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-copy /Y "D:\zzh\Minecraft plugin\BetterServer\target\BetterServer-1.0.jar" "D:\zzh\Minecraft plugin\BetterServer\run\plugins\"
-java -Xmx1G -Dfile.encoding=UTF-8 -jar leaves-1.20.1.jar nogui
